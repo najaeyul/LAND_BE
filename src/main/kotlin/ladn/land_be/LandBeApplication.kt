263c50dbd3ed48068e29cece8389e264a8d49ea8
yuri <iyoule@naver.com>
2023-12-30 20:52:40 +0900

    Initial commit

---
+++ src/main/kotlin/ladn/land_be/LandBeApplication.kt
@@ -0,0 +1,11 @@
+package ladn.land_be
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LandBeApplication
+
+fun main(args: Array<String>) {
+    runApplication<LandBeApplication>(*args)
+}
---
+++ src/test/kotlin/ladn/land_be/LandBeApplicationTests.kt
@@ -0,0 +1,13 @@
+package ladn.land_be
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LandBeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
