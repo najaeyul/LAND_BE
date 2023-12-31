8a8778fbec72f87c64abe7be2b947979ea9de663
yuri <iyoule@naver.com>
2023-12-31 23:28:41 +0900

    feat: login 작성중

---
+++ src/main/kotlin/ladn/land_be/config/SecurityConfig.kt
@@ -0,0 +1,82 @@
+package ladn.land_be.config
+
+import ladn.land_be.config.oauth2.CustomOAuth2ClientProperties
+import ladn.land_be.config.oauth2.CustomOAuth2Provider
+import lombok.RequiredArgsConstructor
+import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2ClientProperties
+import org.springframework.context.annotation.Bean
+import org.springframework.security.config.Customizer.withDefaults
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.oauth2.client.CommonOAuth2Provider
+import org.springframework.security.oauth2.client.registration.ClientRegistration
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.stereotype.Component
+
+
+@Component
+@EnableWebSecurity
+@RequiredArgsConstructor
+class SecurityConfig {
+
+    @Bean
+    @Throws(Exception::class)
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+            .authorizeHttpRequests { authorize ->
+                authorize
+                    .requestMatchers("/", "/login/**", "/oauth2/**", "/images/**").permitAll()
+                    .anyRequest().authenticated()
+            }
+            .httpBasic(withDefaults())
+        return http.build()
+    }
+
+    @Bean
+    fun clientRegistrationRepository(oAuth2ClientProperties: OAuth2ClientProperties,
+                                     customOAuth2ClientProperties: CustomOAuth2ClientProperties
+    ): InMemoryClientRegistrationRepository {
+
+        // 소셜 설정 등록
+        val registrations = oAuth2ClientProperties.registration.keys.mapNotNull {
+            getRegistration(
+                oAuth2ClientProperties,
+                it
+            )
+        }
+            .toMutableList()
+
+        // 추가 설정 프로퍼티
+        val customRegistrations = customOAuth2ClientProperties.registration
+
+        // 추가 소셜 설정을 기본 소셜 설정에 추가
+        if (customRegistrations != null) {
+            for (customRegistration in customRegistrations) {
+
+                when (customRegistration.key) {
+                    "naver" -> registrations.add(CustomOAuth2Provider.NAVER.getBuilder("naver")
+                        .clientId(customRegistration.value.clientId)
+                        .clientSecret(customRegistration.value.clientSecret)
+                        .build())
+                }
+
+            }
+        }
+
+        return InMemoryClientRegistrationRepository(registrations)
+    }
+
+    // 공통 소셜 설정을 호출합니다.
+    private fun getRegistration(clientProperties: OAuth2ClientProperties, client: String): ClientRegistration? {
+        val registration = clientProperties.registration[client]
+        return when(client) {
+            "google" -> CommonOAuth2Provider.GOOGLE.getBuilder(client)
+                .clientId(registration?.clientId)
+                .clientSecret(registration?.clientSecret)
+                .scope("email", "profile")
+                .build()
+            else -> null
+        }
+    }
+}
---
+++ src/main/kotlin/ladn/land_be/config/oauth2/CustomOAuth2ClientProperties.kt
@@ -0,0 +1,21 @@
+package ladn.land_be.config.oauth2
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.stereotype.Component
+
+@Component
+@ConfigurationProperties(prefix = "spring.security.oauth2.client.registration")
+class CustomOAuth2ClientProperties {
+    /**
+     * lateinit : 코틀린에서 늦은 초기화(late initialization)를 위해 사용. non-null 타입의 프로퍼티를 선언할 때 초기값을 주지 않고, 나중에 초기화할 수 있게 해줌
+     */
+    var registration: Map<String, Registration>? = null
+    // lateinit var registration: Map<String, Registration>
+
+    companion object {
+        class Registration {
+            lateinit var clientId: String
+            lateinit var clientSecret: String
+        }
+    }
+}
